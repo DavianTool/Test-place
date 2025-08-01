52a8739a207dff4f2f59d0cb597cb8ba8413cb2c
madRacoon <tool.davian@gmail.com>
2025-08-02 01:29:07 +0300

    Init Skillfactory

--- draft.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-x = [randint(1, 10) for x in range(10)]
-print(x)
-#Продолжаю осваивать Git
---
+++ quadratic_solve.py
@@ -0,0 +1,14 @@
+def D(a, b, c):
+    return b ** 2 - 4 * a * c
+
+def quadratic_solve(a, b, c):
+    if D(a, b, c) < 0:
+        return 'Вещественных корней нет'
+    elif D(a, b, c) == 0:
+        return -b / (2 * a)
+    else:
+        return (-b - D(a,b,c) ** 0.5) / (2 * a), (-b + D(a, b, c) ** 0.5) / (2 * a)
+
+L = list(map(int, input().split()))
+
+print(quadratic_solve(*L))
